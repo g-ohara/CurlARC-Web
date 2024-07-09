88cc7a2fde90335e1b97feb136342741e37e981e
dyoshy <d.yoshhyy@gmail.com>
2024-07-09 19:50:55 +0900

    feat: navigator headerの設定

--- app/(components)/navigator.tsx
+++ app/(components)/navigator.tsx
@@ -1,20 +1,19 @@
 "use client";
+import Link from 'next/link';
 import React from 'react';
 
-const Navigation: React.FC = () => {
+export default function Navigator() {
   return (
     <header className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
         <h1 className="text-4xl font-extrabold leading-none tracking-tight">
-          <a href="/" className="text-white hover:text-gray-300 transition-colors">CurlARC</a>
+          <Link href="/" className="text-white hover:text-gray-300 transition-colors">CurlARC</Link>
         </h1>
         <nav className="ml-auto flex space-x-6">
-          <a href="/login" className="text-lg text-gray-300 hover:text-white transition-colors">Login</a>
-          <a href="/register" className="text-lg text-gray-300 hover:text-white transition-colors">Register</a>
+          <Link href="/login" className="text-lg text-gray-300 hover:text-white transition-colors">Login</Link>
+          <Link href="/register" className="text-lg text-gray-300 hover:text-white transition-colors">Register</Link>
         </nav>
       </div>
     </header>
   );
 };
-
-export default Navigation;

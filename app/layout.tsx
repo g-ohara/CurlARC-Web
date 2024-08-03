391cf6fec60856da364801a7d9178912b696af33
dyoshy <d.yoshhyy@gmail.com>
2024-08-03 18:15:17 +0900

    refac: ディレクトリ構造の更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navigation from "./(components)/navigator";
+import Navigation from "../components/navigator";
 
 const inter = Inter({ subsets: ["latin"] });
 

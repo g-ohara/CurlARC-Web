6af394f8ef3fe04ba35282718c361613718071c6
dyoshyy <d.yoshhyy@gmail.com>
2024-08-10 23:49:48 +0900

    feat: teamCardに切り出し

---
+++ app/team/teamCard.tsx
@@ -0,0 +1,46 @@
+// components/TeamCard.tsx
+import React from 'react'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { TeamHeader } from './teamHeader'
+import { TeamStatistics } from './teamStatistics'
+import { TeamFooter } from './teamFooter'
+import { TeamMembers } from './teamMembers'
+import { TeamDetails } from './teamDetails'
+
+interface TeamCardProps {
+  teamName: string
+  memberCount: number
+  score: { red: number; blue: number }
+  members: Array<{ name: string; role: string }>
+  statisticsData: Array<{ month: string; desktop: number }>
+  teamDetails: Array<{ key: string; value: string }>
+  lastGameDate: string
+}
+
+export function TeamCard({
+  teamName,
+  memberCount,
+  score,
+  members,
+  statisticsData,
+  teamDetails,
+  lastGameDate
+}: TeamCardProps) {
+  return (
+    <Card className="rounded-lg bg-white p-4 shadow-md">
+      <CardHeader>
+        <TeamHeader teamName={teamName} memberCount={memberCount} score={score} />
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-3 gap-6">
+          <TeamMembers className="col-span-1" members={members} />
+          <TeamStatistics className="col-span-1" data={statisticsData} />
+          <TeamDetails data={teamDetails} />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <TeamFooter lastGameDate={lastGameDate} />
+      </CardFooter>
+    </Card>
+  )
+}

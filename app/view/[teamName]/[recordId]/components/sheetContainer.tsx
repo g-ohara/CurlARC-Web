9881beb87740adcb4cb88e0a2247e1befae61f30
dyoshy <d.yoshhyy@gmail.com>
2024-09-05 21:40:00 +0900

    feat: viewにsheetを表示

---
+++ app/view/[teamName]/[recordId]/components/sheetContainer.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Coordinate } from "@/app/@types/model";
+import { Sheet } from "@/components/sheet";
+import { useRef } from "react";
+
+type props = {
+  friendStones: Coordinate[];
+  enemyStones: Coordinate[];
+};
+
+export const SheetContainer = ({friendStones, enemyStones} : props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return (
+    <Sheet
+    canvasRef={canvasRef}
+    width={400}
+    height={400}
+    friendStones={friendStones}
+    enemyStones={enemyStones}
+    friendIsRed={true}
+  />
+  )
+}
--- app/view/[teamName]/[recordId]/page.tsx
+++ app/view/[teamName]/[recordId]/page.tsx
@@ -3,6 +3,8 @@ import ScoreBoard from './components/scoreBoard'
 import StoneCoordinates from './components/stoneCoordinates'
 import { DeleteRecordButton } from './components/DeleteRecordButton'
 import { extractTeamsScoreData } from '@/utils/func/score'
+import { SheetContainer } from './components/sheetContainer'
+import { Coordinate } from '@/app/@types/model'
 
 export default async function RecordPage({ params }: { params: { teamName: string; recordId: string } }) {
   console.log(params)
@@ -10,6 +12,22 @@ export default async function RecordPage({ params }: { params: { teamName: strin
   const recordDetails = res.record
   const teamScoreData = extractTeamsScoreData(recordDetails)
   teamScoreData.friend.team = params.teamName // recordDetailsには自チーム名が含まれていないため、ここで代入
+  const friendStonesDummy : Coordinate[] = [
+    { index: 0, r: 100, theta: 60 },
+    { index: 1, r: 150, theta: 10 },
+    { index: 2, r: 20, theta: 20 },
+    { index: 3, r: 500, theta: 30 },
+    { index: 4, r: 300, theta: 40 },
+    { index: 5, r: 400, theta: 50 },
+  ]
+  const enemyStonesDummy : Coordinate[] = [
+    { index: 0, r: 200, theta: 60 },
+    { index: 1, r: 350, theta: 10 },
+    { index: 2, r: 40, theta: 20 },
+    { index: 3, r: 600, theta: 30 },
+    { index: 4, r: 400, theta: 40 },
+    { index: 5, r: 300, theta: 50 },
+  ]
 
   return (
     <div className="w-full">
@@ -26,6 +44,9 @@ export default async function RecordPage({ params }: { params: { teamName: strin
             <div>{String(recordDetails.date)}</div>
             <ScoreBoard friendScore={teamScoreData.friend} enemyScore={teamScoreData.enemy} />
             <StoneCoordinates />
+            <SheetContainer 
+              friendStones={friendStonesDummy} enemyStones={enemyStonesDummy} 
+            />
           </>
         ) : (
           <div>試合データがありません</div>

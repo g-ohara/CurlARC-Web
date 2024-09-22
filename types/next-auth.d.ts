8a0a005f50fd546b033b0ed4569a350a01f465f2
dyoshy <d.yoshhyy@gmail.com>
2024-09-22 20:45:03 +0900

    fix: 型の拡張

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,4 +4,17 @@ declare module "next-auth" {
   interface Session {
     accessToken?: string;
   }
+
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+  interface JWT {
+    accessToken?: string;
+  }
 }

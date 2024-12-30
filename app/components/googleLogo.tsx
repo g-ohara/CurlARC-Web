c6cbc9ec891791ed5c22c60347e484ba10bbf2b5
dyoshy <d.yoshhyy@gmail.com>
2024-12-30 13:32:26 +0900

    fix: delete login page

--- app/components/googleLogo.tsx
+++ app/components/googleLogo.tsx
@@ -1,18 +1,15 @@
 import React, { SVGProps } from 'react';
 
 interface SignInWithGoogleLogoProps extends JSX.IntrinsicAttributes, SVGProps<SVGSVGElement> {
-  handleFunction: () => void;
+  handleFunction?: () => void;
 }
 
 export function SignInWithGoogleLogo({ handleFunction, ...props }: SignInWithGoogleLogoProps) {
   return (
     <button
       onClick={handleFunction}
-      className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      className="inline-flex items-center justify-center px-4 py-2 rounded-md bg-white "
     >
-      {/* <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute left-0 top-0 w-full h-full bg-white opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-10"></div>
-      </div> */}
       <div className="relative flex items-center space-x-4">
         <div className="flex-shrink-0 w-5 h-5">
           <svg

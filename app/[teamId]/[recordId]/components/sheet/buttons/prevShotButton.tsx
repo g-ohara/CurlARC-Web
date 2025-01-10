8d611cf35921e88d62472a52a7f6cda37bd19188
Genji Ohara <oharagenji1024@gmail.com>
2025-01-11 01:44:57 +0900

    perf: restrict prev shot button and next shot button

--- app/[teamId]/[recordId]/components/sheet/buttons/prevShotButton.tsx
+++ app/[teamId]/[recordId]/components/sheet/buttons/prevShotButton.tsx
@@ -9,26 +9,18 @@ type Props = {
 };
 
 export default function PrevShotButton({
-  selectedEndIndex,
-  setSelectedEndIndex,
   selectedShotIndex,
   setSelectedShotIndex,
 }: Props) {
 
   const prevShot = () => {
-    if (selectedShotIndex === 0) {
-      setSelectedShotIndex(15);
-      setSelectedEndIndex(selectedEndIndex - 1);
-    }
-    else {
-      setSelectedShotIndex(selectedShotIndex - 1);
-    }
+    setSelectedShotIndex(selectedShotIndex - 1);
   };
 
   return (
     <CommonButton
       onClick={prevShot}
-      disabled={selectedShotIndex === 0 && selectedEndIndex === 0}
+      disabled={selectedShotIndex === 0}
     >
       <ChevronLeft />
     </CommonButton>
